0b03fe5c00377f78f28070d34b5d34e0b4806b18
Vida Alex <alexvida89@gmail.com>
2018-09-10 14:56:45 +0300

    first commit

---
+++ www/template.js
@@ -0,0 +1,20 @@
+/* global cordova:false */
+/* globals window */
+
+console.log("WORKS");
+var exec = cordova.require('cordova/exec'),
+    utils = cordova.require('cordova/utils');
+
+var template = {
+    echo: function(successCallback, errorCallback, message, forceAsync) {
+        var action = 'echo';
+
+        if (forceAsync) {
+            action += 'Async';
+        }
+
+        exec(successCallback, errorCallback, 'Echo', action, [message]);
+    }
+};
+
+module.exports = template;
